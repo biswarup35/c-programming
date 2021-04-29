ab2fce443a0ddbbf501f3db7b0fad7cfa42e942f
Biswarup35 <biswarup35@gmail.com>
2021-04-29 23:17:17 +0530

    [Updated] Equality operators

--- Equality.c
+++ Equality.c
@@ -1,12 +1,44 @@
 #include "stdio.h"
 int main() {
-    // Demonstrate Equality operator
-    int a = 5, b = 4;
+    /**
+     * Demonstrate Equality operators.
+     * 
+     * This too works as the Relational operators do,
+     * it compares the operands and then if the
+     * expression is true it produces 1, if false it will
+     * produce 0.
+     * 
+     * For eg 5 == 4 this expression is false (coz 4 is less than 5)
+     * Hence, it will produce 0.
+     *      5 != 4, tis expresssion is true.
+     * Hence it will produce 1.
+    */
 
-    // Equals to
-    printf("\n%d == %d = %d", a, b, a == b); // Evaluates 0
+   /**
+    * Declare variables
+    * @param a      First operand
+    * @param b      Second operand
+    * @param result         Expression of 'a' and 'b' operand.
+   */
+    int a, b, result;
+
+    // Take user input
+    printf("\nEnter first number: ");
+    scanf("%d", &a);
+    printf("\nEnter second number: ");
+    scanf("%d", &b);
+
+    /**
+     * Equals to
+     * Note to confused with '=' & '=='
+     * '=' -> Assignment operator
+     * '==' -> equal operator
+    */
+    result = a == b;
+    printf("\n%d == %d = %d", a, b, result);
 
     // Not equal to
-    printf("\n%d != %d = %d", a, b, a != b); // Evaluates 1
+    result = a != b;
+    printf("\n%d != %d = %d", a, b, result);
     return 0;
 }
